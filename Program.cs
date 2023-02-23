ef27f4e14140a3b86c77f4077a0dca825aa755f1
shubhampachghare <shubhampachghare@gmail.com>
2023-02-23 23:25:49 +0530

    [Shubham]-SinglePlayerGameStart

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+
+        const int START_POSITION = 0;
+        const int END_POSITION = 100;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("start position is: " + START_POSITION);
+            Console.WriteLine("end position is: " + END_POSITION);
+        }
+    }
+}
