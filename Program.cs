7d8bf097563450e5d1e06c2b45fba0d1247cb741
shubhampachghare <shubhampachghare@gmail.com>
2023-02-23 23:38:23 +0530

    [Shubham]-GetOption

--- Program.cs
+++ Program.cs
@@ -7,20 +7,47 @@ namespace SnakeAndLadder
         const int START_POSITION = 0;
         const int END_POSITION = 100;
         public static int currentPosition = 0;
+        public static int selectdOption = 0;
+        static Random random = new Random();
 
         public static int throwDice()
         {
-            Random random = new Random();
-            int number = random.Next(1, 7);
-            return number;
+            return random.Next(1, 7);
+        }
+
+        public static int checkForOption()
+        {
+            return random.Next(1, 4);
+        }
+
+        public static int playerOption()
+        {
+            int throwDices = throwDice();
+            int selectdOption = checkForOption();
+            if (selectdOption == 2)
+            {
+                currentPosition += throwDices;
+            }
+            else if (selectdOption == 3)
+            {
+                currentPosition -= throwDices;
+                if (currentPosition < 0)
+                {
+                    currentPosition = START_POSITION;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Do nothing player stay in same poistion");
+            }
+            return currentPosition;
         }
 
         static void Main(string[] args)
         {
-            currentPosition += throwDice();
-            Console.WriteLine("Current position is=" + currentPosition);
             Console.WriteLine("start position is: " + START_POSITION);
             Console.WriteLine("end position is: " + END_POSITION);
+            Console.WriteLine("current position is= " + playerOption());
         }
     }
 }
